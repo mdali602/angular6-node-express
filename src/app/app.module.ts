a2b4020c4a21bd9ffc2951dca48c89cc7d5ab03b
m.ali <m.ali@indicsoft.com>
2019-01-01 18:12:37 +0530

    initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'angular-universal-demo'}),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: 'home', component: HomeComponent, pathMatch: 'full'},
+      { path: 'projects', component: ProjectsComponent, pathMatch: 'full'},
+      { path: 'contact', component: ContactComponent, pathMatch: 'full'},
+    ]),
     FormsModule,
     HttpModule
   ],
